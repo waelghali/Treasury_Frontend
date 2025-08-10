7a89e5aa81614b2e90b17a87e79f77c7f95860cc
Wael Ghali <waelghali79@gmail.com>
2025-08-10 17:34:47 +0300

    Frontend changes craco2

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,18 @@
-// In craco.config.js
+const webpack = require('webpack');
+
 module.exports = {
+  // Your existing craco configuration
+  // ...
+
+  webpack: {
+    plugins: [
+      // This plugin makes the environment variable available in the build process
+      new webpack.DefinePlugin({
+        'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL),
+      }),
+    ],
+  },
+  // Your existing craco configuration for PostCSS
   style: {
     postcss: {
       plugins: [
